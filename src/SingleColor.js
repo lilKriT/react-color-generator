235430b204fce0f2dabcd1517816b830e19f40d4
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-03 23:54:50 +0200

    Added copy to clipboard. Added an alert.

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,13 +5,26 @@ const SingleColor = ({ rgb, weight, index, hex }) => {
   const bcg = rgb.join(",");
   const hexValue = `#${hex}`;
 
+  useEffect(() => {
+    const alertTimeout = setTimeout(() => setAlert(false), 3000);
+
+    return () => {
+      clearTimeout(alertTimeout);
+    };
+  }, [alert]);
+
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
     </article>
   );
 };
